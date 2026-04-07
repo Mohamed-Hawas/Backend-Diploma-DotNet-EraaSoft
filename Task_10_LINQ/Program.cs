54da665c562508bdf704a174eaeb023de4d502ad
Mohamed Hawas <muhamed.muhamed.hawas@gmail.com>
2026-04-07 22:32:19 +0200

    intial commit task 10

---
+++ Task_10_LINQ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_10_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
