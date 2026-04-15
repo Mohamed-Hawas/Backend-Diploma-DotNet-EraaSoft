d075b032c1d9edfc9e632b131e35e2d0398d1811
Mohamed Hawas <muhamed.muhamed.hawas@gmail.com>
2026-04-15 22:23:35 +0200

    add task 11

---
+++ Task_11_Book_My_Doctor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Task_11_Book_My_Doctor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
